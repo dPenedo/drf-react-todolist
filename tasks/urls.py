35c2895731d31d4cccff45565d92bde24f9d7387
daniel <dpenedo@tutanota.com>
2024-12-02 15:16:06 -0300

    django and react application

---
+++ tasks/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+
+router.register(r"tasks", views.TaskView, "tasks")
+
+# Api versioning
+urlpatterns = [
+    path("api/v1/", include(router.urls)),
+    path("docs/", include_docs_urls(title="Tasks API")),
+]
